06e13a9323ca81ab39a705f7e029016069559687
Terry Case <terrylcase@gmail.com>
2015-07-07 23:33:24 -0700

    Fixed documentation note

--- lib/storage_visualizer.rb
+++ lib/storage_visualizer.rb
@@ -26,7 +26,7 @@ class StorageVisualizer
   # Static
   def self.print_usage
     puts "\nThis tool helps visualize which directories are occupying the most storage. Any directory that occupies more than 5% of disk space is added to a visual hierarchichal storage report in the form of a Google Sankey diagram. The storage data is gathered using the linux `du` utility. It has been tested on Mac OSX, should work on linux systems, will not work on Windows. Run as sudo if analyzing otherwise inaccessible directories. May take a while to run\n"
-    puts "\nCommand line usage: \n\t[sudo] ./visualize_storage[.rb] [directory to visualize (default ~/) | -h (help) -i | --install (install to /usr/local/bin)]\n\n"
+    puts "\nCommand line usage: \n\t[sudo] storage_visualizer[.rb] [directory to visualize (default ~/) | -h (help) -i | --install (install to /usr/local/bin)]\n\n"
     puts "API usage: "
     puts "\tgem install storage_visualizer"
     puts "\t'require storage_visualizer'"
